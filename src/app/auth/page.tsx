e62cbc9c02726c53d9ebeedadfa95c6d6e1cf449
Gavin Hensley <gavinhensley@protonmail.com>
2025-05-03 16:47:36 -0400

    Add logic to check for any posts in Feed

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -14,7 +14,7 @@ export default function Page() {
         supabase.auth.signInWithOAuth({
             provider,
             options: {
-                redirectTo: location.origin + "/auth/callback",
+                redirectTo: location.origin + "/dashboard",
             },
         });
     };
--- src/app/dashboard/feed/Feed.js
+++ src/app/dashboard/feed/Feed.js
@@ -17,16 +17,13 @@ export default async function Feed() {
   return (
     <div className="bg-gradient-to-b from-[#1d1d1d] to-[#86059F] rounded-md shadow p-3 h-full text-white">
       <h1 className="font-semibold mb-2">Your Feed</h1>
-{posts.map((post) => (
-  <div key={post.id}>
-    <p>{post.profiles.name} 
-      </p>
-      <p>{post.content}
-        </p>
+      {posts && posts.map((post) => (
+        <div key={post.id}>
+        <p>{post.profiles.name}</p>
+        <p>{post.content}</p>
         <Likes post={post} user={user} />
         </div>
         ))}
-    
     </div>
   );
 }
