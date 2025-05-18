1311406f460fccea9173adc37c34de234d2002cf
Brenda Hensley <renaeehensley@gmail.com>
2025-05-18 19:01:39 -0400

    Ran Prettier to format files
    Formatted 41 files

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -1,18 +1,14 @@
 import { NextResponse } from "next/server";
 // The client you created from the Server-Side Auth instructions
 import { createClient } from "../../../../utils/supabase/server";
-import { isSafeNextPath } from "@/lib/utils";
-
 export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url);
   const code = searchParams.get("code");
-
-  const rawNext = searchParams.get("next") ?? "/";
-  const next = isSafeNextPath(rawNext) ? rawNext : "/";
+  // if "next" is in param, use it as the redirect URL
+  const next = searchParams.get("next") ?? "/";
   if (code) {
     const supabase = await createClient();
     const { error } = await supabase.auth.exchangeCodeForSession(code);
-
     if (!error) {
       const forwardedHost = request.headers.get("x-forwarded-host"); // original origin before load balancer
       const isLocalEnv = process.env.NODE_ENV === "development";
