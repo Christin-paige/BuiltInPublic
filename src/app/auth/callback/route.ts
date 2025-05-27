fb19aa97e00a09af8e493cce1f19c3c4013de548
Gavin Hensley <gavinhensley@protonmail.com>
2025-05-27 10:57:55 -0400

    Fix email sign in logic and ran prettier

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -1,20 +1,20 @@
-import { NextResponse } from "next/server";
+import { NextResponse } from 'next/server';
 // The client you created from the Server-Side Auth instructions
-import { createClient } from "../../../../utils/supabase/server";
-import { isSafeNextPath } from "@/lib/utils";
+import { createClient } from '../../../../utils/supabase/server';
+import { isSafeNextPath } from '@/lib/utils';
 
 export async function GET(request: Request) {
   const { searchParams, origin } = new URL(request.url);
-  const code = searchParams.get("code");
+  const code = searchParams.get('code');
 
-  const rawNext = searchParams.get("next") ?? "/";
-  const next = isSafeNextPath(rawNext) ? rawNext : "/";
+  const rawNext = searchParams.get('next') ?? '/';
+  const next = isSafeNextPath(rawNext) ? rawNext : '/';
   if (code) {
     const supabase = await createClient();
     const { error } = await supabase.auth.exchangeCodeForSession(code);
     if (!error) {
-      const forwardedHost = request.headers.get("x-forwarded-host"); // original origin before load balancer
-      const isLocalEnv = process.env.NODE_ENV === "development";
+      const forwardedHost = request.headers.get('x-forwarded-host'); // original origin before load balancer
+      const isLocalEnv = process.env.NODE_ENV === 'development';
       if (isLocalEnv) {
         // we can be sure that there is no load balancer in between, so no need to watch for X-Forwarded-Host
         return NextResponse.redirect(`${origin}${next}`);
