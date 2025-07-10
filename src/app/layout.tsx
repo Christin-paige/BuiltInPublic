04ed7bb369d3c351d6377f71d2c3eada606dbd94
Gavin Hensley <gavinhensley@protonmail.com>
2025-07-10 19:43:12 -0400

    Remove query provider from public layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import "../app/globals.css";
 import Footer from "../components/footer";
 import { ThemeProvider } from "../components/providers/themes-provider";
-import QueryProvider from "@/components/providers/query-provider";
 
 export default async function RootLayout({
   children,
@@ -11,17 +10,15 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body>
-        <QueryProvider>
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="dark"
-            enableSystem
-            disableTransitionOnChange
-          >
-            {children}
-            <Footer />
-          </ThemeProvider>
-        </QueryProvider>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+          <Footer />
+        </ThemeProvider>
       </body>
     </html>
   );
