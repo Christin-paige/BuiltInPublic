43a12977a1100158ea5478224357af8438bdd512
Nicholas Clark <nicholas.a.clark@outlook.com>
2025-07-11 19:56:56 -0400

    Fixing up the dashboard page.tsx file as I put the home page here instead

--- src/app/(main)/profile/page.tsx
+++ src/app/(main)/profile/page.tsx
@@ -1,32 +1,41 @@
-"use client";
+// app/(main)/dashboard/Dashboard.tsx
 
-import React from "react";
-import "./profile.css";
-import Image from "next/image";
-import FeedSection from "./components/FeedSection";
-import UserInfo from "./components/UserInfo";
-import StreakSection from "./components/StreakSection";
-import GradientBlobs from "./components/GradientBlobs";
+import React, { FC } from "react";
+import Projects from "./projects/ProjectList";
+import FriendsProjects from "./friends-projects/FriendsProjects";
+import Stats from "./stats/Stats";
+import Feed from "./feed/Feed";
+import Profile from "./profile/Profile";
+import Groups from "./groups/Groups";
 
-const Profile = (): JSX.Element => {
+const Dashboard: FC = async () => {
   return (
-    <main className="h-screen flex flex-col py-16 items-center gap-8 -z-10 overflow-hidden">
-      <Image
-        src="/example-cover-img.jpg"
-        alt="Cover Photo"
-        width={2000}
-        height={1200}
-        className="w-full h-48 object-cover hover:opacity-80 transition-all duration-100 transform-content object-top"
-      />
-
-      <div className="flex p-8 gap-12 w-full relative">
-        <GradientBlobs />
-        <UserInfo />
-        <FeedSection />
-        <StreakSection />
+    <div className="grid grid-cols-1 lg:grid-cols-6 gap-3 pt-45 p-4 overflow-hidden bg-gray-900 text-white">
+      <div className="row-span-1 row-start-1 flex flex-col overflow-auto">
+        <Profile />
       </div>
-    </main>
+
+      <div className="row-span-3 row-start-2 flex flex-col overflow-auto">
+        <Projects />
+      </div>
+
+      <div className="col-span-4 flex flex-col overflow-auto">
+        <FriendsProjects />
+      </div>
+
+      <div className="col-span-1 overflow-auto">
+        <Stats />
+      </div>
+
+      <div className="col-span-1 col-start-6 overflow-auto">
+        <Groups />
+      </div>
+
+      <div className="lg:col-start-2 lg:col-span-4 row-start-2 flex flex-col h-[650px] overflow-auto">
+        <Feed />
+      </div>
+    </div>
   );
 };
 
-export default Profile;
+export default Dashboard;
