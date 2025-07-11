d201a98c17f7c38bb7fcf2fefa2dba10dbc69893
Gavin Hensley <gavinhensley@protonmail.com>
2025-07-10 22:42:46 -0400

    Add comments to main/layout

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -2,6 +2,8 @@ import "../../app/globals.css";
 import { ThemeProvider } from "@/components/providers/themes-provider";
 import QueryProvider from "@/components/providers/query-provider";
 
+// This layout doesn't use a <html> or <body> tag because it's nested
+// inside the root layout which already includes those tags.
 export default async function MainLayout({
   children,
 }: {
