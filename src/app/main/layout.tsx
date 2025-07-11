1e4d0f2dda88d72eccd83df4608633e11fff42b5
Gavin Hensley <gavinhensley@protonmail.com>
2025-07-10 20:12:24 -0400

    Remove html and body from main layout as that caused hydration errors

--- src/app/main/layout.tsx
+++ src/app/main/layout.tsx
@@ -1,26 +1,21 @@
-import "../app/globals.css";
-import { ThemeProvider } from "@/components/providers/themes-provider";
-import QueryProvider from "@/components/providers/query-provider";
+import '../../app/globals.css';
+import { ThemeProvider } from '@/components/providers/themes-provider';
+import QueryProvider from '@/components/providers/query-provider';
 
-export default async function RootLayout({
+export default async function MainLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>
-        <QueryProvider>
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="dark"
-            enableSystem
-            disableTransitionOnChange
-          >
-            {children}
-          </ThemeProvider>
-        </QueryProvider>
-      </body>
-    </html>
+    <QueryProvider>
+      <ThemeProvider
+        attribute='class'
+        defaultTheme='dark'
+        enableSystem
+        disableTransitionOnChange>
+        {children}
+      </ThemeProvider>
+    </QueryProvider>
   );
 }
