636a633499ab33ff97b55d2e12e47a0b7bc588d0
Brenda Hensley <renaeehensley@gmail.com>
2025-07-13 14:50:45 -0400

    Change KOA sameSite cookie setting to secure and lax

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -25,7 +25,11 @@ export async function updateSession(request: NextRequest) {
             request,
           });
           cookiesToSet.forEach(({ name, value, options }) =>
-            supabaseResponse.cookies.set(name, value, options),
+            supabaseResponse.cookies.set(name, value, {
+              sameSite: "lax",
+              secure: true,
+              ...options,
+            }),
           );
         },
       },
