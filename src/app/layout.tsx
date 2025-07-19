03f96dcbb868194b599b539f2e88b7c3db482512
Gagandeep <igagandeep95@gmail.com>
2025-07-19 12:36:15 -0400

    copied placeholder metadata into dev and update favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,44 @@ import "../app/globals.css";
 import Footer from "../components/Footer";
 import { ThemeProvider } from "../components/Providers/ThemeProvider";
 
+export const metadata = {
+  title: "Built In Public",
+  description:
+    "Join our supportive community where developers collaborate, share progress, and grow together",
+  keywords: ["relevant", "keywords", "for", "your", "project"],
+  authors: [{ name: "Built In Public" }],
+  openGraph: {
+    title: "Built In Public",
+    description:
+      "Join our supportive community where developers collaborate, share progress, and grow together",
+    url: "https://www.builtinpublic.tech/",
+    siteName: "Built In Public",
+    images: [
+      {
+        url: "/og-image.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Built In Public",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Built In Public",
+    description:
+      "Join our supportive community where developers collaborate, share progress, and grow together",
+    images: ["/og-image.jpg"],
+  },
+  robots: {
+    index: true,
+    follow: true,
+  },
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
+
 export default async function RootLayout({
   children,
 }: {
