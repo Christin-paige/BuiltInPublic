560dd0db54bc667eda5525de597c1ea4d0d183e1
Brenda Hensley <renaeehensley@gmail.com>
2025-07-19 17:46:01 -0400

    Change double to single quotes on friendsprojects.tsx

--- src/app/(main)/dashboard/friends-projects/FriendsProjects.tsx
+++ src/app/(main)/dashboard/friends-projects/FriendsProjects.tsx
@@ -1,10 +1,10 @@
 const FriendsProjects = () => {
   return (
-    <div className="bg-gradient-to-b from-[#1d1d1d] to-[#86059F] rounded-md shadow p-3 h-full text-white">
-      <h1 className="font-semibold mb-2">{`Friends' Projects`}</h1>
+    <div className='bg-gradient-to-b from-[#1d1d1d] to-[#86059F] rounded-md shadow p-3 h-full text-white'>
+      <h1 className='font-semibold mb-2'>{`Friends' Projects`}</h1>
 
-      <div className="w-full max-w-4xl">
-        <p className="text-lg">No projects yet. Start building!</p>
+      <div className='w-full max-w-4xl'>
+        <p className='text-lg'>No projects yet. Start building!</p>
       </div>
     </div>
   );
