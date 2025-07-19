6fc4d583eb3b49859059598599d353ebb64707da
Brenda Hensley <renaeehensley@gmail.com>
2025-07-19 16:46:32 -0400

    Ran prettier

--- scripts/precheck.sh
+++ scripts/precheck.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+# Skip checks if the latest commit has [skip-precheck]
+LAST_COMMIT_MSG=$(git log -1 --pretty=%B)
+if echo "$LAST_COMMIT_MSG" | grep -qi '\[skip-precheck\]'; then
+  echo "⚠️ Skipping pre-push checks due to [skip-precheck] tag in last commit."
+  exit 0
+fi
 
 echo "🔍 Running Pre-PR Security & Code Quality Checks..."
 
