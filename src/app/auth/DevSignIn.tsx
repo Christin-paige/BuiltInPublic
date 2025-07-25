b4406d98da65a1c483194c2ca19bf18a681df768
vgajjar <vgajjar22@gmail.com>
2025-07-25 12:38:02 +0100

    fixes to import new button component

--- src/app/auth/DevSignIn.tsx
+++ src/app/auth/DevSignIn.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Button } from '../../../@/components/ui/button';
+import { Button } from "@/components/ui/button"
 import { LoginWithEmail } from './actions';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
