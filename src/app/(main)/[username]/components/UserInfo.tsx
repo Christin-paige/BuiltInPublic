a9eb7dd3705e50a4664eae6208190a772b11eff9
Brenda Hensley <renaeehensley@gmail.com>
2025-07-27 15:55:03 -0400

    Remove Followers, Following, Posts and counts from left sidebar

--- src/app/(main)/[username]/components/UserInfo.tsx
+++ src/app/(main)/[username]/components/UserInfo.tsx
@@ -18,31 +18,6 @@ export default function UserInfo({ profile }: UserInfoProps) {
           adipisicing elit. Quisquam, quos.
         </p>
       </div>
-      <div className='grid grid-cols-2 gap-4'>
-        <div className='bg-slate-950 p-2 rounded-lg border text-center'>
-          <h2 className='text-xl'>Followers</h2>
-          <p className='text-lg'>100</p>
-        </div>
-        <div className='bg-slate-950 p-2 rounded-lg border text-center'>
-          <h2 className='text-xl'>Following</h2>
-          <p className='text-lg'>100</p>
-        </div>
-        <div className='bg-slate-950 p-2 rounded-lg border text-center'>
-          <h2 className='text-xl'>Projects</h2>
-          <p className='text-lg'>100</p>
-        </div>
-        <div className='bg-slate-950 p-2 rounded-lg border text-center'>
-          <h2 className='text-xl'>Posts</h2>
-          <p className='text-lg'>100</p>
-        </div>
-      </div>
-      <button
-        className='bg-slate-950 p-2 rounded-lg border text-center w-fit cursor-pointer hover:bg-slate-800 transition-all
-            duration-100 active:scale-95 flex items-center gap-2'
-      >
-        <Settings className='w-6 h-6' />
-        <p>Settings</p>
-      </button>
     </section>
   );
 }
