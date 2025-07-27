a5c19ca63049062a1e3b7053ed8c0d754eb1f898
Nicholas Clark <nicholas.a.clark@outlook.com>
2025-07-27 15:43:03 -0400

    style: auto-fix linting and formatting issues [skip-precheck]

--- src/app/(main)/[username]/components/FeedSection.tsx
+++ src/app/(main)/[username]/components/FeedSection.tsx
@@ -3,11 +3,7 @@ import { useState } from 'react';
 const FeedSection = () => {
   const [activeTab, setActiveTab] = useState<'posts' | 'projects'>('posts');
 
-  return (
-    <section className='flex flex-col gap-4 w-2/4'>
-    
-    </section>
-  );
+  return <section className='flex flex-col gap-4 w-2/4'></section>;
 };
 
 export default FeedSection;
