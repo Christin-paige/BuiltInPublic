7fa55f1487b019df640cc35f5cb9d6f07220e7b9
vgajjar <vgajjar22@gmail.com>
2025-07-28 15:44:33 +0100

    fixes to specify the onboarding form as being client side

--- src/app/(main)/onboarding/onboarding-form/OnboardingForm.tsx
+++ src/app/(main)/onboarding/onboarding-form/OnboardingForm.tsx
@@ -1,8 +1,19 @@
+
+"use client";
+
+import { useState } from 'react';
+
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Textarea } from '@/components/ui/textarea';
 
 export default function OnboardingForm() {
+
+  const [userName, setUserName] = useState("");
+  const [displayName, setDisplayName] = useState("");
+  const [bio, setBio] = useState("");
+
+
   return (
     <form className='flex flex-col gap-4 w-full max-w-sm items-center'>
       <Input
@@ -11,6 +22,7 @@ export default function OnboardingForm() {
         type='text'
         placeholder='username'
         required
+        value={userName}
       />
       <Input
         className='text-center'
@@ -18,8 +30,13 @@ export default function OnboardingForm() {
         type='text'
         placeholder='display name'
         required
+        value={displayName}
       />
-      <Textarea className='text-center' placeholder='bio' />
+      <Textarea 
+        className='text-center' 
+        placeholder='bio' 
+        value={bio}
+      />
       <Button
         variant='outline'
         className='w-1/2 flex items-center gap-2 p-2 cursor-pointer justify-center'
