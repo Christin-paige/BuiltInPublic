2562d7264385af6d55c8f34121bb2558a9f0ff85
vgajjar <vgajjar22@gmail.com>
2025-07-28 15:46:06 +0100

    style: auto-fix linting and formatting issues [skip-precheck]

--- src/app/(main)/onboarding/onboarding-form/OnboardingForm.tsx
+++ src/app/(main)/onboarding/onboarding-form/OnboardingForm.tsx
@@ -1,5 +1,4 @@
-
-"use client";
+'use client';
 
 import { useState } from 'react';
 
@@ -8,11 +7,9 @@ import { Input } from '@/components/ui/input';
 import { Textarea } from '@/components/ui/textarea';
 
 export default function OnboardingForm() {
-
-  const [userName, setUserName] = useState("");
-  const [displayName, setDisplayName] = useState("");
-  const [bio, setBio] = useState("");
-
+  const [userName, setUserName] = useState('');
+  const [displayName, setDisplayName] = useState('');
+  const [bio, setBio] = useState('');
 
   return (
     <form className='flex flex-col gap-4 w-full max-w-sm items-center'>
@@ -32,11 +29,7 @@ export default function OnboardingForm() {
         required
         value={displayName}
       />
-      <Textarea 
-        className='text-center' 
-        placeholder='bio' 
-        value={bio}
-      />
+      <Textarea className='text-center' placeholder='bio' value={bio} />
       <Button
         variant='outline'
         className='w-1/2 flex items-center gap-2 p-2 cursor-pointer justify-center'
