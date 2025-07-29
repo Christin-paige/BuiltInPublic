33f1dfdb7c8ecaeb6e76f115208edd88af7e14c1
vgajjar <vgajjar22@gmail.com>
2025-07-29 21:26:49 +0100

    style: auto-fix linting and formatting issues [skip-precheck]

--- src/app/(main)/onboarding/onboarding-form/OnboardingForm.tsx
+++ src/app/(main)/onboarding/onboarding-form/OnboardingForm.tsx
@@ -14,12 +14,11 @@ import {
   FormItem,
   FormLabel,
   FormMessage,
-} from "@/components/ui/form"
+} from '@/components/ui/form';
 
-
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { z } from "zod"
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
 
 import emojiRegex from 'emoji-regex';
 
@@ -30,83 +29,85 @@ export default function OnboardingForm() {
   const [errors, setErrors] = useState<string[]>([]);
 
   const onboardingFormSchema = z.object({
-    userName: 
-      z.string().max(32, {
-      message: 'username cannot be longer than 32 characters'
-    })
-    // only allow lowercase letters, numbers and the following symbols: _-
-    .regex(/^[a-z0-9_-]+$/, {
-      message: 'username must only contain lowercase letters, numbers, _ and - symbols'
-    }).refine((name) => !emojiRegex().test(name), {
-      message: 'username must not contain emoji'
-    }),
+    userName: z
+      .string()
+      .max(32, {
+        message: 'username cannot be longer than 32 characters',
+      })
+      // only allow lowercase letters, numbers and the following symbols: _-
+      .regex(/^[a-z0-9_-]+$/, {
+        message:
+          'username must only contain lowercase letters, numbers, _ and - symbols',
+      })
+      .refine((name) => !emojiRegex().test(name), {
+        message: 'username must not contain emoji',
+      }),
     displayName: z.string().max(32, {
-      message: 'display name cannot be longer than 32 characters'
+      message: 'display name cannot be longer than 32 characters',
     }),
     bio: z.string().max(300, {
-      message: 'bio cannot cannot be longer than 300 characters'
-    })
-  })
+      message: 'bio cannot cannot be longer than 300 characters',
+    }),
+  });
 
   const onboardingForm = useForm<z.infer<typeof onboardingFormSchema>>({
-      resolver: zodResolver(onboardingFormSchema),
-      defaultValues: {
-        userName: "",
-        displayName: "",
-        bio: ""
-      }
+    resolver: zodResolver(onboardingFormSchema),
+    defaultValues: {
+      userName: '',
+      displayName: '',
+      bio: '',
+    },
   });
 
   const onSubmit = (values: z.infer<typeof onboardingFormSchema>) => {
-   
     console.log(values);
   };
 
   return (
     <Form {...onboardingForm}>
-    <form
-      onSubmit={onboardingForm.handleSubmit(onSubmit)}
-      className='flex flex-col gap-4 w-full max-w-sm items-center'
-    >
-      <Input
-        id='userName'
-        type='text'
-        placeholder='username'
-        required
-        value={userName}
-        maxLength={32}
-        onChange={(e) => setUserName(e.target.value)}
-      />
-      <Input
-        id='displayName'
-        type='text'
-        placeholder='display name'
-        required
-        value={displayName}
-        maxLength={32}
-        onChange={(e) => setDisplayName(e.target.value)}
-      />
-      <Textarea
-        placeholder='bio'
-        value={bio}
-        maxLength={300}
-        onChange={(e) => setBio(e.target.value)}
-      />
-      {errors.length > 0 && (
-        <ul className='text-red-500 text-sm list-disc pl-5'>
-          {errors.map((err, i) => (
-            <li key={i}>{err}</li>
-          ))}
-        </ul>
-      )}
-      <Button
-        variant='outline'
-        className='w-1/2 flex items-center gap-2 p-2 cursor-pointer justify-center'
-        type='submit'
+      <form
+        onSubmit={onboardingForm.handleSubmit(onSubmit)}
+        className='flex flex-col gap-4 w-full max-w-sm items-center'
       >
-        submit
-      </Button>
-    </form>
+        <Input
+          id='userName'
+          type='text'
+          placeholder='username'
+          required
+          value={userName}
+          maxLength={32}
+          onChange={(e) => setUserName(e.target.value)}
+        />
+        <Input
+          id='displayName'
+          type='text'
+          placeholder='display name'
+          required
+          value={displayName}
+          maxLength={32}
+          onChange={(e) => setDisplayName(e.target.value)}
+        />
+        <Textarea
+          placeholder='bio'
+          value={bio}
+          maxLength={300}
+          onChange={(e) => setBio(e.target.value)}
+        />
+        {errors.length > 0 && (
+          <ul className='text-red-500 text-sm list-disc pl-5'>
+            {errors.map((err, i) => (
+              <li key={i}>{err}</li>
+            ))}
+          </ul>
+        )}
+        <Button
+          variant='outline'
+          className='w-1/2 flex items-center gap-2 p-2 cursor-pointer justify-center'
+          type='submit'
+        >
+          submit
+        </Button>
+      </form>
     </Form>
   );
 }
