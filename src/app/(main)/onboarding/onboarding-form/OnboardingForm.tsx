8e8cbaeb887d070da3085b996b926d62b96e5dc3
vgajjar <vgajjar22@gmail.com>
2025-07-29 15:42:00 +0100

    first attempt at adding form validation on the username field

--- src/app/(main)/onboarding/onboarding-form/OnboardingForm.tsx
+++ src/app/(main)/onboarding/onboarding-form/OnboardingForm.tsx
@@ -1,18 +1,54 @@
 'use client';
 
-import { useState } from 'react';
+import { FormEvent, useState } from 'react';
 
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Textarea } from '@/components/ui/textarea';
 
+import emojiRegex from 'emoji-regex';
+
 export default function OnboardingForm() {
   const [userName, setUserName] = useState('');
   const [displayName, setDisplayName] = useState('');
   const [bio, setBio] = useState('');
+  const [errors, setErrors] = useState<string[]>([]);
 
+
+  const handleSubmit = (e:FormEvent) => {
+
+    e.preventDefault();
+
+    const newErrors: string[] = [];
+
+    // validate userName;
+    const emojiValidationRegex = emojiRegex();
+    const allowedCharactersRegex = /^[a-z0-9_-]+$/;
+
+    if (userName.length > 32 ){
+      const lengthError = "username cannot be longer than 32 characters";
+      newErrors.push(lengthError);
+    }
+
+    if ( emojiValidationRegex.test(userName)){
+      const emojiError = "username must not contain emoji";
+      newErrors.push(emojiError);
+    }
+
+    if ( !allowedCharactersRegex.test(userName)){
+      const alphaNumericError = "username must only contain lowercase letters, numbers, _ and - symbols"
+      newErrors.push(alphaNumericError);
+    }
+
+    setErrors(newErrors);
+
+    console.log(newErrors);
+
+
+  }
+ 
   return (
-    <form className='flex flex-col gap-4 w-full max-w-sm items-center'>
+    <form onSubmit={handleSubmit} className='flex flex-col gap-4 w-full max-w-sm items-center'>
       <Input
         className='text-center'
         id='userName'
@@ -20,6 +56,7 @@ export default function OnboardingForm() {
         placeholder='username'
         required
         value={userName}
+        onChange={(e) => setUserName(e.target.value)}
       />
       <Input
         className='text-center'
@@ -28,13 +65,22 @@ export default function OnboardingForm() {
         placeholder='display name'
         required
         value={displayName}
+        onChange={(e) => setDisplayName(e.target.value)}
       />
       <Textarea
         className='text-center'
         placeholder='bio'
         value={bio}
         maxLength={300}
+        onChange={(e => setBio(e.target.value))}
       />
+      {errors.length > 0 && (
+        <ul className="text-red-500 text-sm list-disc pl-5">
+          {errors.map((err, i) => (
+            <li key={i}>{err}</li>
+          ))}
+        </ul>
+      )}
       <Button
         variant='outline'
         className='w-1/2 flex items-center gap-2 p-2 cursor-pointer justify-center'
