1e263d7d0bc71dddf3d8172630e67c0bebdda5da
vgajjar <vgajjar22@gmail.com>
2025-07-31 14:36:21 +0100

    move the Zod schema to it's own file

--- src/app/(main)/onboarding/onboarding-form/OnboardingForm.tsx
+++ src/app/(main)/onboarding/onboarding-form/OnboardingForm.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import { onboardingFormSchema } from './onboarding-form.schema';
+
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Textarea } from '@/components/ui/textarea';
@@ -17,37 +19,8 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import { z } from 'zod';
 
-import emojiRegex from 'emoji-regex';
 
 export default function OnboardingForm() {
-  const onboardingFormSchema = z.object({
-    userName: z
-      .string()
-      .min(3, {
-        message: 'username must contain at least 3 characters',
-      })
-      .max(32, {
-        message: 'username cannot be longer than 32 characters',
-      })
-      .refine(
-        (name) => /^[a-z0-9_-]+$/.test(name) && !emojiRegex().test(name),
-        {
-          message:
-            'username must only contain lowercase letters, numbers, _ and - symbols',
-        }
-      ),
-    displayName: z
-      .string()
-      .min(3, {
-        message: 'display name must contain at least 3 characters',
-      })
-      .max(32, {
-        message: 'display name cannot be longer than 32 characters',
-      }),
-    bio: z.string().max(300, {
-      message: 'bio cannot cannot be longer than 300 characters',
-    }),
-  });
 
   const onboardingForm = useForm<z.infer<typeof onboardingFormSchema>>({
     resolver: zodResolver(onboardingFormSchema),
---
+++ src/app/(main)/onboarding/onboarding-form/onboarding-form.schema.tsx
@@ -0,0 +1,31 @@
+  import emojiRegex from 'emoji-regex';
+  import { z } from 'zod';
+  
+  export const onboardingFormSchema = z.object({
+    userName: z
+      .string()
+      .min(3, {
+        message: 'username must contain at least 3 characters',
+      })
+      .max(32, {
+        message: 'username cannot be longer than 32 characters',
+      })
+      .refine(
+        (name) => /^[a-z0-9_-]+$/.test(name) && !emojiRegex().test(name),
+        {
+          message:
+            'username must only contain lowercase letters, numbers, _ and - symbols',
+        }
+      ),
+    displayName: z
+      .string()
+      .min(3, {
+        message: 'display name must contain at least 3 characters',
+      })
+      .max(32, {
+        message: 'display name cannot be longer than 32 characters',
+      }),
+    bio: z.string().max(300, {
+      message: 'bio cannot cannot be longer than 300 characters',
+    }),
+  });
