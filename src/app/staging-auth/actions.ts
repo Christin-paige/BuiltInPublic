c2496be81c9c929897fdb14e7bea8c6424788307
Andrew Couture <coudrew@gmail.com>
2025-08-01 14:25:29 -0300

    removes console log

--- src/app/staging-auth/actions.ts
+++ src/app/staging-auth/actions.ts
@@ -6,7 +6,7 @@ import { StagingAuthSchemaType } from './stagingAuth.schema';
 
 export async function authenticateStaging(formData: StagingAuthSchemaType) {
   const { password } = formData;
-  console.log(password);
+
   if (password === process.env.NEXT_PUBLIC_STAGING_PASS) {
     (await cookies()).set('staging-auth', 'true', {
       maxAge: 60 * 60 * 2,
