575a2501c0c651ff1f2d75d8c5864bf1876dab14
Andrew Couture <coudrew@gmail.com>
2025-08-05 22:14:11 -0300

    uses timingSafeEqual for staging password comparison

--- src/app/staging-auth/actions.ts
+++ src/app/staging-auth/actions.ts
@@ -3,11 +3,17 @@
 import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation';
 import { StagingAuthSchemaType } from './stagingAuth.schema';
+import { timingSafeEqual } from 'crypto';
 
 export async function authenticateStaging(formData: StagingAuthSchemaType) {
   const { password } = formData;
 
-  if (password === process.env.NEXT_PUBLIC_STAGING_PASS) {
+  if (
+    timingSafeEqual(
+      Buffer.from(password),
+      Buffer.from(process.env.NEXT_PUBLIC_STAGING_PASS!)
+    )
+  ) {
     (await cookies()).set('staging-auth', 'true', {
       maxAge: 60 * 60 * 2,
       httpOnly: true,
