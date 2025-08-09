5c8bec27f2a750007b863736b3d4d935c26097e3
David Weiss <me@davidfweiss.com>
2025-08-09 14:21:51 -0400

    Add link instead

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,13 @@
-import { SuggestFeatureButton } from './SuggestFeatureButton';
-
 export default function Footer() {
   return (
     <div className='hidden md:block w-full'>
-      <SuggestFeatureButton />
+      <a
+        href='https://builtinpublic.features.vote/board'
+        target='_blank'
+        className='underline hover:no-underline'
+      >
+        Suggest a feature
+      </a>
       <h1>About</h1>
       <h1>Questions?</h1>
     </div>
