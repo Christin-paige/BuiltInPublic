3410246dd3233c8aef3feca2d1907c5846b03d13
David Weiss <me@davidfweiss.com>
2025-08-09 12:43:10 -0400

    Add suggest feature button

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,9 @@
+import { SuggestFeatureButton } from './SuggestFeatureButton';
+
 export default function Footer() {
   return (
     <div className='hidden md:block w-full'>
+      <SuggestFeatureButton />
       <h1>About</h1>
       <h1>Questions?</h1>
     </div>
