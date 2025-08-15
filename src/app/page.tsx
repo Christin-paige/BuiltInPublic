f888280bcfc35097c40a559cafc52c0c3844c0a8
Gavin Hensley <gavinhensley@protonmail.com>
2025-08-14 23:16:17 -0400

    Fix design of the cards at the bottom of the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import Link from 'next/link';
 
 const Home: FC = () => {
   return (
-    <section className='px-4 md:px-10 py-20 w-full h-full min-h-screen relative bg-primary-950/40'>
+    <section className='px-4 md:px-10 py-20 w-full h-full min-h-screen relative bg-primary-950/30'>
       <div className='absolute top-0 left-0 size-40 md:size-100 xl:size-120 rounded-full bg-radial from-sky-500/60 to-indigo-900/60 via-indigo-900/60 blur-3xl'></div>
       <div className='absolute top-[15%] md:top-[20%] lg:top-[20%] left-[-10] md:left-[25%] xl:left-[30%] w-full md:w-1/2 xl:w-2/5 h-1/5 lg:h-1/4 xl:h-1/3 rounded-full bg-radial from-pink-500/60 to-indigo-600/60 blur-3xl'></div>
       <div className='relative pt-[20vh] h-full bg-cover bg-center flex flex-col items-center justify-center pb-8 gap-20'>
@@ -53,18 +53,18 @@ const Home: FC = () => {
           </div>
         </div>
 
-        <div className='grid grid-cols-1 md:grid-cols-3 w-full lg:w-2/3 gap-x-8 xl:gap-x-20 gap-y-8'>
-          <div className='flex flex-col text-center border-2 border-cyan-500 p-4 flex-1'>
+        <div className='grid grid-cols-1 md:grid-cols-3 w-full lg:w-2/3 gap-x-8 xl:gap-x-20 gap-y-8 text-text-300'>
+          <div className='flex flex-col text-center border-2 border-secondary-700 p-4 flex-1 bg-secondary-950/50'>
             <p className='font-bold text-xl'>1</p>
             <p className='font-bold text-xl'>Create a build log</p>
             <p>Start sharing your project</p>
           </div>
-          <div className='flex flex-col text-center border-2 border-purple-500 p-4 flex-1'>
+          <div className='flex flex-col text-center border-2 border-primary-700 p-4 flex-1 bg-primary-950/50'>
             <p className='font-bold text-xl'>2</p>
             <p className='font-bold text-xl'>Engage</p>
             <p>Get feedback, give feedback</p>
           </div>
-          <div className='flex flex-col text-center border-2 border-rose-500 p-4 flex-1'>
+          <div className='flex flex-col text-center border-2 border-accent-700 p-4 flex-1 bg-accent-950/50'>
             <p className='font-bold text-xl'>3</p>
             <p className='font-bold text-xl'>Grow</p>
             <p>Track your progress, learn from others</p>
