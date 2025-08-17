d33ca0dc27337bb2d1f0a9277eada2241fc0ef75
Andrew Couture <coudrew@gmail.com>
2025-08-17 19:02:27 -0300

    refactors project types

--- src/repositories/projectRepository/project.repository.ts
+++ src/repositories/projectRepository/project.repository.ts
@@ -1,5 +1,5 @@
 import { BaseRepository } from '@/repositories/base.repository';
-import { AnySupabaseClient } from 'utils/supabase/server';
+import { AnySupabaseClient, isServiceRoleClient } from 'utils/supabase/server';
 import { Project, ProjectDTO } from './project.types';
 
 // This class extends the BaseRepository to provide specific methods for the Project entity
@@ -13,7 +13,7 @@ export class ProjectRepository extends BaseRepository<ProjectDTO, Project> {
     const query = this.supabase
       .from('projects')
       .select(
-        '*, owner:profiles(username), updates(*)',
+        '*, owner:profiles(id, username), updates:project_updates(*)',
         count ? { count: 'exact' } : undefined
       );
 
@@ -44,10 +44,41 @@ export class ProjectRepository extends BaseRepository<ProjectDTO, Project> {
       description: description || '',
       visibility,
       status,
-      repoUrl: external_url || '',
+      externalUrl: external_url || '',
       createdAt,
       updates: updates || [],
     } satisfies Project;
+  }
+
+  async getPublicProjectById(id: string): Promise<Project | null> {
+    if (!isServiceRoleClient(this.supabase)) {
+      throw new Error('Service Role Client required');
+    }
+    try {
+      const query = this.getBaseQuery();
+
+      const { data, error } = await this.applyFilters(query, {
+        id,
+        visibility: 'public',
+      }).maybeSingle();
+
+      if (error) {
+        throw error;
+      }
+
+      if (!data) {
+        throw new Error('Project not found');
+      }
+
+      const project = this.safeTransformDTO(data);
+
+      return project;
+    } catch (e) {
+      console.error(
+        `Failed to fetch project with: ${JSON.stringify(e, null, 2)} id: ${id}`
+      );
+      throw e;
+    }
   }
 
   async getById(id: string): Promise<Project | null> {
@@ -59,6 +90,7 @@ export class ProjectRepository extends BaseRepository<ProjectDTO, Project> {
       }).maybeSingle();
 
       if (error) {
+        console.error(error);
         throw error;
       }
 
