b6c44f03ed9a0816a6a4db99d8224cd69f7d63a7
Gavin Hensley <gavinhensley@protonmail.com>
2025-08-22 10:53:14 -0400

    Finish adjusting the responsiveness for the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,9 @@ import Link from 'next/link';
 
 const Home: FC = () => {
   return (
-    <section className='px-4 md:px-10 pt-24 sm:pt-36 xl:pt-54 gap-16 w-full min-h-screen relative bg-primary-950/30 flex flex-col items-center'>
+    <section className='px-4 pb-8 md:px-10 pt-24 sm:pt-36 xl:pt-54 gap-16 xl:gap-20 w-full min-h-screen relative bg-primary-950/30 flex flex-col items-center'>
       <div className='absolute top-0 left-0 size-40 md:size-100 xl:size-120 rounded-full bg-radial from-sky-500/60 to-indigo-900/60 via-indigo-900/60 blur-3xl z-0'></div>
-      <div className='absolute top-[15%] md:top-[20%] lg:top-[20%] left-[-10] md:left-[25%] xl:left-[30%] w-full md:w-1/2 xl:w-2/5 h-1/5 lg:h-1/4 xl:h-1/3 rounded-full bg-radial from-pink-500/60 to-indigo-600/60 blur-3xl z-0'></div>
+      <div className='absolute top-[7%] md:top-[20%] lg:top-[16%] xl:top-[20%] left-[-10] md:left-[25%] xl:left-[30%] w-full md:w-1/2 xl:w-2/5 h-1/5 lg:h-1/4 xl:h-1/3 rounded-full bg-radial from-pink-500/60 to-indigo-600/60 blur-3xl z-0'></div>
       
       <div className='flex flex-col items-center gap-4 md:gap-8 z-10 relative'>
         <header className='flex flex-col items-center gap-2 text-center z-10 relative'>
@@ -56,11 +56,21 @@ const Home: FC = () => {
         </div>
       </div>
 
-      <div>
-        <div className='flex flex-col text-center border-2 border-secondary-700 p-4 flex-1 bg-secondary-950/50 max-w-md min-w-xs'>
+      <div className='flex justify-center gap-6 md:gap-8 xl:gap-24 flex-wrap w-full'>
+        <div className='flex flex-col text-center border-2 border-secondary-700 p-4 bg-secondary-950/50 w-xs shadow-md shadow-black/50'>
           <p className='font-bold text-xl'>1</p>
           <p className='font-bold text-xl'>Create a build log</p>
           <p>Start sharing your project</p>
+        </div>
+        <div className='flex flex-col text-center border-2 border-primary-700 p-4 bg-primary-950/50 w-xs shadow-md shadow-black/50'>
+          <p className='font-bold text-xl'>2</p>
+          <p className='font-bold text-xl'>Engage</p>
+          <p>Get feedback, give feedback</p>
+        </div>
+        <div className='flex flex-col text-center border-2 border-accent-700 p-4 bg-accent-950/50 w-xs shadow-md shadow-black/50'>
+          <p className='font-bold text-xl'>3</p>
+          <p className='font-bold text-xl'>Grow</p>
+          <p>Track your progress, learn from others</p>
         </div>
       </div>
 
