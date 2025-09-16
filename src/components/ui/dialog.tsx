1100616661a5ebc864bf8534b96c48179aceddf3
Gavin Hensley <gavinhensley@protonmail.com>
2025-09-16 12:30:15 -0400

    Add blur to dialog

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -62,7 +62,7 @@ function DialogContent({
       <DialogPrimitive.Content
         data-slot='dialog-content'
         className={cn(
-          'bg-secondary-950/50 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 fixed top-[50%] left-[50%] z-50 grid w-full max-w-[calc(100%-2rem)] translate-x-[-50%] translate-y-[-50%] gap-4 rounded-lg border p-6 shadow-lg duration-300 sm:max-w-lg border-secondary-900/70 transition-all hover:border-secondary-800/80 focus:outline-hidden focus:ring focus:ring-offset-2 focus:ring-ring focus:ring-offset-background dark:border-secondary-900/70 dark:hover:border-secondary-800/80 dark:focus:ring-secondary-800 ease-in-out',
+          'bg-secondary-950/50 backdrop-blur-md data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 fixed top-[50%] left-[50%] z-50 grid w-full max-w-[calc(100%-2rem)] translate-x-[-50%] translate-y-[-50%] gap-4 rounded-lg border p-6 shadow-lg duration-300 sm:max-w-lg border-secondary-900/70 transition-all hover:border-secondary-800/80 focus:outline-hidden focus:ring focus:ring-offset-2 focus:ring-ring focus:ring-offset-background dark:border-secondary-900/70 dark:hover:border-secondary-800/80 dark:focus:ring-secondary-800 ease-in-out',
           className
         )}
         {...props}
