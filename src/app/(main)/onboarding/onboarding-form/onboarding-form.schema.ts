575ea584411d11751ee51789e0a7b9530be84fe5
Gavin Hensley <gavinhensley@protonmail.com>
2025-09-17 14:16:34 -0400

    style: auto-format code with Prettier [skip-precheck]

--- src/app/(main)/onboarding/onboarding-form/onboarding-form.schema.ts
+++ src/app/(main)/onboarding/onboarding-form/onboarding-form.schema.ts
@@ -3,18 +3,17 @@ import { z } from 'zod';
 
 export const onboardingFormSchema = z.object({
   userName: z
-      .string()
-      .min(3, { message: 'Username must contain at least 3 characters' })
-      .max(32, { message: 'Username cannot be longer than 32 characters' })
-      .refine((name) => /^[a-z0-9_-]+$/.test(name) && !emojiRegex().test(name),
-        {
-          message:
-            'Username must only contain lowercase letters, numbers, _ and - symbols',
-        }),
+    .string()
+    .min(3, { message: 'Username must contain at least 3 characters' })
+    .max(32, { message: 'Username cannot be longer than 32 characters' })
+    .refine((name) => /^[a-z0-9_-]+$/.test(name) && !emojiRegex().test(name), {
+      message:
+        'Username must only contain lowercase letters, numbers, _ and - symbols',
+    }),
   displayName: z
-      .string()
-      .min(3, { message: 'Display name must contain at least 3 characters' })
-      .max(32, { message: 'Display name cannot be longer than 32 characters' }),
+    .string()
+    .min(3, { message: 'Display name must contain at least 3 characters' })
+    .max(32, { message: 'Display name cannot be longer than 32 characters' }),
   bio: z
     .string()
     .max(300, { message: 'Bio cannot be longer than 300 characters' })
