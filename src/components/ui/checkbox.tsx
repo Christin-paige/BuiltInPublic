b8c4d95a6fe7ecc6af9f0d377e083c5e5c33f500
Andrew Couture <coudrew@gmail.com>
2025-12-03 22:15:36 -0400

    style: auto-format code with Prettier [skip-precheck]

--- src/components/ui/checkbox.tsx
+++ src/components/ui/checkbox.tsx
@@ -4,7 +4,8 @@ import * as React from 'react';
 import { Check } from 'lucide-react';
 import { cn } from '@/lib/utils';
 
-export interface CheckboxProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+export interface CheckboxProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {}
 
 const Checkbox = React.forwardRef<HTMLInputElement, CheckboxProps>(
   ({ className, ...props }, ref) => (
